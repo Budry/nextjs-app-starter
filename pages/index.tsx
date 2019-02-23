84eba93c461dcc72b37b203b4321039511ee4370
Budry <zarubaondra@gmail.com>
2019-02-23 12:10:49 +0100

    Added basic source

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+/**
+ * This file is part of the nextjs-app-starter package.
+ *
+ * (c) Ondřej Záruba <info@zaruba-ondrej.cz>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+import * as React from 'react';
+import {FunctionComponent} from 'react';
+
+const index: FunctionComponent<{}> = () => {
+    return (
+        <div>
+            <h1>Hello world!</h1>
+        </div>
+    );
+};
+
+export default index;
